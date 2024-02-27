a574826fe9e3d57867021c883e0ed99ed3ff841d
NickHatone <152749196+NickHatone@users.noreply.github.com>
2024-02-26 21:48:07 -0300

    initial.commit

---
+++ config.js
@@ -0,0 +1,25 @@
+// 霸都丶傲天 2019年12月24日
+
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "quería decirte algo antes de ir a descansar, siempre enfrentas los obstáculos con una resiliencia admirable,
+         y eso te convierte en una persona llena de ideas, como esas que muchas veces me ayudaron y me siguen ayudando,
+          cuáles me inspiran y también lo hacen a los que están a tu alrededor como una antorch,
+          ver como interactuas con tus seres queridos es genuinamente conmovedor,
+          espero Dios te abrace el resto de la eternidad, y estés en su roca firme e inamovible,
+          deseo que estés mejor cada día tanto de salud como en tu dominio propio, eres una rosa en medio de un jardín, 
+          eres un pilar en tu familia no dudes en tu esperanza, no me iré aún que los días sean súper difíciles y hasta tu misma 
+          no sepas que hacer, no me iré por querer verte y no poder hacerlo en ocasiones especiales o dificultosas, aquí tienes a 
+          
+        alguien totalmente entregado para tratar de sacarte de la rutina y mil sonrisas, prometo algun día besarte cada mañana sin
+         faltar ningún día, cuando venga la tempestad Dios pondrá sus manos sobre ti, y te sostendrá por qué le perteneces y nada saldrá
+          de su control, no habrá más oscuridad, buenas noches cielo te amo un montón 🫶🏻
+    
+    ],
+    wish: "小可爱&&大傻子相爱了",  //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+    // 时间的格式很重要哦，一定要是下面这个格式!!
+    // 年年年年-月月-日日 时时:分分:秒秒
+    time: "2017-07-20 0:0:0",    //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+};
